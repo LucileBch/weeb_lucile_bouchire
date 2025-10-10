8be536702de78259ecc60ae18c48097b68b05a6b
LucileBch <l.bouchire@gmail.com>
2025-10-10 16:37:32 +0200

    style(component): fix filled button smaller effect because of border

--- src/components/buttons/FilledButton.tsx
+++ src/components/buttons/FilledButton.tsx
@@ -22,7 +22,7 @@ export function FilledButton({
         duration: 0.3,
         ease: "easeInOut",
       }}
-      className="text-responsive inline-block cursor-pointer rounded-lg bg-[var(--color-purple-bg)] px-2 py-1 transition-all duration-300 lg:px-3 lg:py-3"
+      className="text-responsive inline-block cursor-pointer rounded-lg border-2 border-[var(--color-purple-bg)] bg-[var(--color-purple-bg)] px-2 py-1 transition-all duration-300 lg:px-3 lg:py-3"
       onClick={onClick}
     >
       {label}
