e1bc2b9d56454e676d5db94798bd9c23138fc92d
LucileBch <l.bouchire@gmail.com>
2026-02-18 18:50:48 +0100

    feat(interceptor): axios request and response interceptor

--- weeb_frontend/src/core/api/axiosInstance.ts
+++ weeb_frontend/src/core/api/axiosInstance.ts
@@ -1,9 +1,7 @@
 // ---------- AXIOS INSTANCE ---------- //
-// for future add here :
-// auth
-// refresh token
-// interceptors
 import axios from "axios";
+import { getCookie } from "../utils/helpers";
+import { endpoints } from "./endpoints";
 
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
@@ -13,3 +11,56 @@ export const api = axios.create({
     Accept: "application/json",
   },
 });
+
+/**
+ * Request Interceptor
+ * Inject access tokens for request.
+ */
+api.interceptors.request.use(
+  (config) => {
+    const accessToken = getCookie("access_token");
+
+    if (accessToken) {
+      config.headers["Authorization"] = `Bearer ${accessToken}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+/**
+ * Response Interceptor
+ * Handle token refresh automatically when access token expires (401).
+ */
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        await axios.post(endpoints.refreshToken, {}, { withCredentials: true });
+
+        const newAccessToken = getCookie("access_token");
+        if (newAccessToken) {
+          originalRequest.headers["Authorization"] = `Bearer ${newAccessToken}`;
+        }
+
+        return api(originalRequest);
+      } catch (refreshError) {
+        console.error("Refresh token expiré, redirection login...");
+        window.dispatchEvent(new Event("force-logout"));
+        return Promise.reject(refreshError);
+      }
+    }
+
+    return Promise.reject(error);
+  },
+);
