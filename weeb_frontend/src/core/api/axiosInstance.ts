98f7bc058bf7c1cf0e99120cc2012208b2ae738e
LucileBch <l.bouchire@gmail.com>
2026-02-18 19:00:42 +0100

    feat(config): add time out in axios interceptor

--- weeb_frontend/src/core/api/axiosInstance.ts
+++ weeb_frontend/src/core/api/axiosInstance.ts
@@ -6,6 +6,7 @@ import { endpoints } from "./endpoints";
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
+  timeout: 30000,
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
@@ -39,14 +40,34 @@ api.interceptors.response.use(
   (response) => {
     return response;
   },
+
   async (error) => {
     const originalRequest = error.config;
 
-    if (error.response?.status === 401 && !originalRequest._retry) {
+    if (error.code === "ECONNABORTED") {
+      console.error("La requête a mis trop de temps à répondre.");
+      return Promise.reject(
+        new Error("Le serveur met trop de temps à répondre."),
+      );
+    }
+
+    const isAuthRoute =
+      originalRequest.url?.includes(endpoints.refreshToken) ||
+      originalRequest.url?.includes(endpoints.login);
+
+    if (
+      error.response?.status === 401 &&
+      !originalRequest._retry &&
+      !isAuthRoute
+    ) {
       originalRequest._retry = true;
 
       try {
-        await axios.post(endpoints.refreshToken, {}, { withCredentials: true });
+        await axios.post(
+          `${import.meta.env.VITE_API_URL}${endpoints.refreshToken}`,
+          {},
+          { withCredentials: true },
+        );
 
         const newAccessToken = getCookie("access_token");
         if (newAccessToken) {
@@ -55,10 +76,14 @@ api.interceptors.response.use(
 
         return api(originalRequest);
       } catch (refreshError) {
-        console.error("Refresh token expiré, redirection login...");
+        console.error("Session expirée, déconnexion en cours...");
         window.dispatchEvent(new Event("force-logout"));
         return Promise.reject(refreshError);
       }
+    }
+
+    if (error.response?.status === 401 && isAuthRoute) {
+      window.dispatchEvent(new Event("force-logout"));
     }
 
     return Promise.reject(error);
