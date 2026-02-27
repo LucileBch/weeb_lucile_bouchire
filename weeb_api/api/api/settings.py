c1edeaa11883a7be58e5442995efe71efb54a0eb
LucileBch <l.bouchire@gmail.com>
2026-02-27 16:01:26 +0100

    chore(database): config supabase i/o sqlite

--- weeb_api/api/api/settings.py
+++ weeb_api/api/api/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 from datetime import timedelta
+from decouple import config
 
 load_dotenv()
 
@@ -142,8 +143,15 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
+        'OPTIONS': {
+            'sslmode': 'require'
+        },
     }
 }
 
--- weeb_api/api/users/urls.py
+++ weeb_api/api/users/urls.py
@@ -2,12 +2,13 @@ from rest_framework.routers import DefaultRouter
 from django.urls import path, include
 from .views import RegisterViewSet, MyTokenObtainPairView, LogoutView, MyTokenRefreshView, ForgotPasswordCodeRequestView, ForgotPasswordConfirmView, UserProfileUpdateView
 
-router = DefaultRouter()
+# router = DefaultRouter()
 
-router.register(r'signup', RegisterViewSet, basename='register')
+# router.register(r'signup', RegisterViewSet, basename='register')
 
 urlpatterns = [
-    path('', include(router.urls)),
+    # path('', include(router.urls)),
+    path('signup/', RegisterViewSet.as_view({'post': 'create'}), name='register'),
 
     path('auth/login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/refresh-token/', MyTokenRefreshView.as_view(), name='token_refresh'),
